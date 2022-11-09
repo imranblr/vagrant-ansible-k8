ab715f610287071f292a62770aaf96e18998ee11
Imran Khan <imrankhan.blr@gmail.com>
2022-11-09 11:06:58 +0300

    moving to github

---
+++ ansible/k8master/Dockerfile
@@ -0,0 +1,2 @@
+FROM docker:dind
+COPY gitlab.example.com.crt /etc/ssl/certs/
---
+++ ansible/k8master/update-nfs-share.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ if [ -f /nfs-share ]; then  mkdir /nfs-share; else echo folder exists; fi
+
+grep nfs-share /etc/fstab
+if [ $? == 0 ]; then echo fstab already has entry; else echo "192.168.100.13:/nfs-share /nfs-share nfs auto,nofail,noatime,nolock,intr,tcp 0 0" >>/etc/fstab; fi
+
+mount -a
+df -h
+exit
